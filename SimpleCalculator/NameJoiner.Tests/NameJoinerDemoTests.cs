cc516c016e2322ead17f94ade6161c395ffe9e64
bradonf333 <bradonf123@gmail.com>
2018-02-18 19:58:38 -0700

    Added new NameJoinerDemo project along with the test project. Created 1 test.

---
+++ SimpleCalculator/NameJoiner.Tests/NameJoinerDemoTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NameJoiner.Tests
+{
+    [TestFixture]
+    public class NameJoinerDemoTests
+    {
+        [Test]
+        public void ShouldJoinNames()
+        {
+            var sut = new NameJoinerDemo();
+
+            var fullName = sut.Join("Sarah", "Smith");
+
+            Assert.That(fullName, Is.EqualTo("Sarah Smith"));
+        }
+    }
+}
---
+++ SimpleCalculator/NameJoiner.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NameJoiner.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NameJoiner.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5fa7ec16-db32-4601-9243-bc25e3a7d0bd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
