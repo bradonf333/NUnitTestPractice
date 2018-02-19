746ada95fb9ab445b8d692f1c6fc7ff41ab2ad57
bradonf333 <bradonf123@gmail.com>
2018-02-18 20:17:23 -0700

    Specified Add to Ints only and added a AddDoubles method

--- Calculator/SimpleCalculator.cs
+++ Calculator/SimpleCalculator.cs
@@ -8,7 +8,12 @@ namespace Calculator
 {
     public class SimpleCalculator
     {
-        public int Add(int a, int b)
+        public int AddInts(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double AddDoubles(double a, double b)
         {
             return a + b;
         }
