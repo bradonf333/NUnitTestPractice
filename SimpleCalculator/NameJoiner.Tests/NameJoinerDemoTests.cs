e5a5b20cd1f4e3be259b435e23a8e6d93b1d6f44
bradonf333 <bradonf123@gmail.com>
2018-02-18 20:14:07 -0700

    Added 2 new tests

--- SimpleCalculator/NameJoiner.Tests/NameJoinerDemoTests.cs
+++ SimpleCalculator/NameJoiner.Tests/NameJoinerDemoTests.cs
@@ -15,5 +15,25 @@ namespace NameJoiner.Tests
 
             Assert.That(fullName, Is.EqualTo("Sarah Smith"));
         }
+
+        [Test]
+        public void ShouldJoinNames_CaseInsensitive()
+        {
+            var sut = new NameJoinerDemo();
+
+            var fullName = sut.Join("sarah", "smith");
+
+            Assert.That(fullName, Is.EqualTo("Sarah Smith").IgnoreCase);
+        }
+
+        [Test]
+        public void ShouldJoinNames_NotEqual()
+        {
+            var sut = new NameJoinerDemo();
+
+            var fullName = sut.Join("Sarah", "Smith");
+
+            Assert.That(fullName, Is.Not.EqualTo("Bob Sap"));
+        }
     }
 }
