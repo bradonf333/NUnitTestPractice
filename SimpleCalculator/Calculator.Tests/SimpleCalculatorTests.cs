be96950cab51673c9a5af4503b68a526549ee974
bradonf333 <bradonf123@gmail.com>
2018-02-18 21:23:45 -0700

    Added tests for tolerance and Is.Not.EqualTo

--- SimpleCalculator/Calculator.Tests/SimpleCalculatorTests.cs
+++ SimpleCalculator/Calculator.Tests/SimpleCalculatorTests.cs
@@ -6,13 +6,43 @@ namespace Calculator.Tests
     public class SimpleCalculatorTests
     {
         [Test]
-        public void ShouldAddTwoNumbers()
+        public void ShouldAddTwoInts()
         {
             var sut = new SimpleCalculator();
 
-            var result = sut.Add(1, 2);
+            var result = sut.AddInts(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ShouldAddTwoDoubles()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.AddDoubles(1.1, 2.2);
+
+            Assert.That(result, Is.Not.EqualTo(3.3));
+        }
+
+        [Test]
+        public void ShouldAddTwoDoubles_WithTolerance()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.AddDoubles(1.1, 2.2);
+
+            Assert.That(result, Is.EqualTo(3.3).Within(0.01));
+        }
+
+        [Test]
+        public void ShouldAddTwoDoubles_WithPercentTolerance()
+        {
+            var sut = new SimpleCalculator();
+
+            var result = sut.AddDoubles(50, 50);
+
+            Assert.That(result, Is.EqualTo(101).Within(1).Percent);
         }
 
         [Test]
