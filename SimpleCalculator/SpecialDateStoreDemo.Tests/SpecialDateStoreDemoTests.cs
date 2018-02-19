4c64bdaa40b35b6d1cdf45bd887ee50f2d664d19
bradonf333 <bradonf123@gmail.com>
2018-02-19 13:43:33 -0700

    Added simple Date class and test methods

---
+++ SimpleCalculator/SpecialDateStoreDemo.Tests/SpecialDateStoreDemoTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace SpecialDateStoreDemo.Tests
+{
+    [TestFixture]
+    public class SpecialDateStoreDemoTests
+    {
+        [Test]
+        public void ShouldHaveCorrectMilleniumDate()
+        {
+            var sut = new SepcialDateStore();
+
+            var result = sut.DateOf(SpecialDates.NewMillennium);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2000, 1, 1, 0, 0, 0, 0)));
+        }
+
+        [Test]
+        public void ShouldHaveIncorrectMilleniumDateMillisecond()
+        {
+            var sut = new SepcialDateStore();
+
+            var result = sut.DateOf(SpecialDates.NewMillennium);
+
+            Assert.That(result, Is.Not.EqualTo(new DateTime(2000, 1, 1, 0, 0, 0, 1)));
+        }
+
+        [Test]
+        public void ShouldHaveCorrectMilleniumDate_WithTolerance()
+        {
+            var sut = new SepcialDateStore();
+
+            var result = sut.DateOf(SpecialDates.NewMillennium);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2000, 1, 1, 0, 0, 0, 1))
+                                  .Within(TimeSpan.FromMilliseconds(1)));
+        }
+    }
+}
---
+++ SimpleCalculator/SpecialDateStoreDemo/SepcialDateStore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SpecialDateStoreDemo
+{
+    public class SepcialDateStore
+    {
+        public DateTime DateOf(SpecialDates specialDate)
+        {
+            switch(specialDate)
+            {
+                case SpecialDates.NewMillennium:
+                    return new DateTime(2000, 1, 1, 0, 0, 0, 0);
+                case SpecialDates.NewCentury:
+                    return new DateTime(2100, 1, 1, 0, 0, 0, 0);
+                default:
+                    throw new ArgumentOutOfRangeException("specialDate");
+            }
+
+        }
+    }
+
+    public enum SpecialDates
+    {
+        NewMillennium,
+        NewCentury
+    }
+}
