78184aa51c51679614b18e574dc99cec0e6423b9
bradonf333 <bradonf123@gmail.com>
2018-02-19 15:09:29 -0700

    PlayerCharacter and Tests

---
+++ SimpleCalculator/PlayerCharacterApp.Tests/PlayerCharacterTests.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace PlayerCharacterApp.Tests
+{
+    [TestFixture]
+    public class PlayerCharacterTests
+    {
+        [Test]
+        public void ShouldIncreaseHealthAfterSleeping()
+        {
+            var sut = new PlayerCharacter { Health = 100 };
+
+            sut.Sleep();
+
+            Assert.That(sut.Health, Is.GreaterThan(100));
+        }
+
+        [Test]
+        public void ShouldIncreaseHealthInExpectedRangeAfterSleeping()
+        {
+            var sut = new PlayerCharacter { Health = 100 };
+
+            sut.Sleep();
+
+            Assert.That(sut.Health, Is.InRange(101, 200));
+        }
+
+        [Test]
+        public void ShouldHaveDefaultRandomGeneratedName()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.That(sut.Name, Is.Not.Empty);
+        }
+
+        [Test]
+        public void ShouldHaveDefaultNullNickName()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.That(sut.NickName, Is.Null);
+        }
+
+        [Test]
+        public void ShouldBeNoob()
+        {
+            var sut = new PlayerCharacter();
+
+            Assert.That(sut.IsNoob, Is.True);
+        }
+    }
+}
---
+++ SimpleCalculator/PlayerCharacterApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PlayerCharacterApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PlayerCharacterApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("98881052-3e6c-4a6f-8081-7696e5d9bd25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
