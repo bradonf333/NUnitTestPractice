47f342af1285c9d87d2d4a7cb253877dbddb74a6
bradonf333 <bradonf123@gmail.com>
2018-02-20 19:15:29 -0700

    Add Test

---
+++ SimpleCalculator/MemoryCalculatorApp.Tests/MemoryCalculatorAppTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MemoryCalculatorApp.Tests
+{
+    [TestFixture]
+    public class MemoryCalculatorAppTests
+    {
+        MemoryCalculator sut = new MemoryCalculator();
+
+        [Test]
+        public void ShouldAdd()
+        {
+            sut.Add(10);
+            sut.Add(5);
+
+            Assert.That(sut.CurrentValue, Is.EqualTo(15));
+        }
+    }
+}
---
+++ SimpleCalculator/MemoryCalculatorApp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MemoryCalculatorApp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MemoryCalculatorApp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b9f71510-7904-42f5-a072-b059dae452d3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
